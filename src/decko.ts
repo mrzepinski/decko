3187a4f70e86c44d8e03c63109913e59c55d2c5d
mrzepinski <Maciej.Rzepinski@wolterskluwer.com>
2020-09-08 10:28:31 +0200

    Move to TypeScript

--- src/decko.ts
+++ src/decko.ts
@@ -1,16 +1,17 @@
-const EMPTY = {};
+const EMPTY: any = {};
 const HOP = Object.prototype.hasOwnProperty;
+
 let fns = {
     /**  let cachedFn = memoize(originalFn); */
     memoize(fn, opt = EMPTY) {
         let cache = opt.cache || {};
         return function (...a) {
             let k = String(a[0]);
-            if (opt.caseSensitive === false)
-                k = k.toLowerCase();
+            if (opt.caseSensitive === false) k = k.toLowerCase();
             return HOP.call(cache, k) ? cache[k] : (cache[k] = fn.apply(this, a));
         };
     },
+
     /** let throttled = debounce(10, console.log); */
     debounce(fn, opts) {
         if (typeof opts === 'function') {
@@ -18,18 +19,19 @@ let fns = {
             fn = opts;
             opts = p;
         }
-        let delay = opts && opts.delay || opts || 0, args, context, timer;
+        let delay = opts && opts.delay || opts || 0,
+            args, context, timer;
         return function (...a) {
             args = a;
             context = this;
-            if (!timer)
-                timer = setTimeout(() => {
-                    fn.apply(context, args);
-                    args = context = timer = null;
-                }, delay);
+            if (!timer) timer = setTimeout(() => {
+                fn.apply(context, args);
+                args = context = timer = null;
+            }, delay);
         };
     },
-    bind(target, key, { value: fn }) {
+
+    bind(target, key, {value: fn}) {
         // In IE11 calling Object.defineProperty has a side-effect of evaluating the
         // getter for the property which is being replaced. This causes infinite
         // recursion and an "Out of stack space" error.
@@ -53,9 +55,11 @@ let fns = {
         };
     }
 };
+
 export const memoize = multiMethod(fns.memoize);
 export const debounce = multiMethod(fns.debounce);
 export const bind = multiMethod((f, c) => f.bind(c), () => fns.bind);
+
 /** Creates a function that supports the following calling styles:
  *    d() - returns an unconfigured decorator
  *    d(opts) - returns a configured decorator
@@ -86,13 +90,15 @@ function multiMethod(inner, deco = inner.decorate || decorator(inner)) {
         return (l < 2 ? deco : (l > 2 ? d : inner))(...args);
     };
 }
+
 /** Returns function supports the forms:
  *    deco(target, key, desc) -> decorate a method
  *    deco(Fn) -> call the decorator proxy on a function
  */
 function decorator(fn) {
-    return opt => (typeof opt === 'function' ? fn(opt) : (target, key, desc) => {
-        desc.value = fn(desc.value, opt, target, key, desc);
-    });
+    return opt => (
+        typeof opt === 'function' ? fn(opt) : (target, key, desc) => {
+            desc.value = fn(desc.value, opt, target, key, desc);
+        }
+    );
 }
-//# sourceMappingURL=decko.js.map
